ac0adf4216bf1313db7b91528cbb5fa5b38b7948
Miikka Van Goethem <miikka.vangoethem@xt-i.com>
2023-11-28 13:03:41 +0100

    Replace lombok with records for TerminalCommand

--- src/main/java/com/example/adventofcode/day7/domain/TerminalCommand.java
+++ src/main/java/com/example/adventofcode/day7/domain/TerminalCommand.java
@@ -1,13 +1,6 @@
 package com.example.adventofcode.day7.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
 import java.util.List;
 
-@Data
-@AllArgsConstructor
-public class TerminalCommand {
-    private TerminalCommandType commandType;
-    private List<String> data;
+public record TerminalCommand (TerminalCommandType commandType, List<String> data){
 }
